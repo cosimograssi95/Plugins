aa74dda53889ae4a9224ccfa57f91ad43bd6870a
cosimograssi95 <cosimo.grassi95@gmail.com>
2025-08-10 12:49:45 +0200

    RetrieveMultipleOverride initial commit

---
+++ RetrieveMultipleOverride/README.md
@@ -0,0 +1,61 @@
+﻿# RetrieveMultipleOverride
+
+## Overview
+
+This Plugin implements **an override for RetrieveMultiple Message** that allows you to query similar records freely within a model driven subgrid.
+This code is meant to serve as a template, feel free to implement your own `customLogic` and `customFilter`. 
+
+
+> ⚠️ https://learn.microsoft.com/en-us/power-apps/developer/data-platform/best-practices/business-logic/limit-registration-plugins-retrieve-retrievemultiple.
+
+---
+
+## How It Works
+
+Given an entity, the Plugin:
+
+1. Intercept the RetrieveMultiple message.
+2. Detect if it is called within a subgrid and retrieves the parent entity guid of the form.
+3. Replace the link entity with your custom filter.
+
+---
+
+## How to create and use the Plugin
+
+### Clone the repository
+
+```bash
+git clone https://github.com/cosimograssi95/Plugins.git
+```
+Build the solution.
+
+### Plugin Registration Tool
+
+If you have PAC CLI installed you can download the tool with
+
+```bash
+pac tool prt
+```
+
+- Connect to your organization
+- `Register > Register new package`.
+
+### Use the Plugin
+
+- Create a new Step on your entity
+
+---
+
+## Example Scenarios
+
+### 1. Show similar records within the same form
+
+You want to show Cases with the same keyword as your currently open Case, or maybe you would like to show all Expenses that happened during the same month as the current open Expense.
+
+---
+
+## Prerequisites
+
+### General Requirements
+- The entity you plan to register this plugin on requires a **Self Referential** relationship, implemented by creating a LookUp column on itself. This column data does not need to be filled nor maintained.
+
---
+++ RetrieveMultipleOverride/RetrieveMultipleOverride.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace CG.Plugins.RetrieveMultipleOverride
+{
+    /// <summary>
+    /// Plugin development guide: https://docs.microsoft.com/powerapps/developer/common-data-service/plug-ins
+    /// Best practices and guidance: https://docs.microsoft.com/powerapps/developer/common-data-service/best-practices/business-logic/
+    /// </summary>
+    public class RetrieveMultipleOverride : PluginBase
+    {
+        public RetrieveMultipleOverride(string unsecureConfiguration, string secureConfiguration)
+            : base(typeof(RetrieveMultipleOverride))
+        {
+            // TODO: Implement your custom configuration handling
+            // https://docs.microsoft.com/powerapps/developer/common-data-service/register-plug-in#set-configuration-data
+        }
+
+        protected override void ExecuteDataversePlugin(ILocalPluginContext localPluginContext)
+        {
+            if (localPluginContext == null)
+            {
+                throw new ArgumentNullException(nameof(localPluginContext));
+            }
+
+            #region getContext
+            IPluginExecutionContext context = localPluginContext.PluginExecutionContext;
+
+            IOrganizationService service = ((IOrganizationServiceFactory)localPluginContext.ServiceProvider.GetService(typeof(IOrganizationServiceFactory))).CreateOrganizationService(null);
+
+            ITracingService tracingService = localPluginContext.TracingService;
+            #endregion
+
+            try
+            {
+                if (context.Mode != 0 || context.Stage != 20 || !context.MessageName.Equals("RetrieveMultiple", StringComparison.OrdinalIgnoreCase) || context.Depth != 1)
+                    return;
+
+                if (!context.InputParameters.Contains("Query") || !(context.InputParameters["Query"] is FetchExpression fetchExpression))
+                    return;
+
+
+                XDocument fetchXmlDocument = XDocument.Parse(fetchExpression.Query);
+                if (fetchXmlDocument.Descendants("link-entity").FirstOrDefault() == null) return;
+
+                XElement linkEntityElement = fetchXmlDocument.Descendants("link-entity").FirstOrDefault();
+                if (linkEntityElement == null) return;
+
+                string linkEntityName = linkEntityElement.Attribute("name").Value;
+                if (linkEntityName != context.PrimaryEntityName) return;
+
+                IEnumerable<XElement> linkEntityFilterElements = linkEntityElement.Descendants("filter");
+
+                XElement linkEntitySelfReferentialCondition =
+                    linkEntityFilterElements
+                        .Descendants("condition")
+                        .Where(condition =>
+                            condition.Attribute("attribute")?.Value.StartsWith(context.PrimaryEntityName, StringComparison.OrdinalIgnoreCase) == true)
+                        .FirstOrDefault();
+
+                if (linkEntitySelfReferentialCondition == null) return;
+
+                if (!Guid.TryParse(linkEntitySelfReferentialCondition.Attribute("value")?.Value, out Guid parentRecordId))
+                    return;
+
+
+                #region customLogic
+                Entity parent = service.Retrieve(context.PrimaryEntityName, parentRecordId, new ColumnSet("new_tablefetchbcategory"));
+
+                OptionSetValue categoryOption = parent.GetAttributeValue<OptionSetValue>("new_tablefetchbcategory");
+                if (categoryOption == null)
+                    return;
+                #endregion
+
+
+                #region customFilter
+
+                XElement customFilter = new XElement("filter",
+
+                    new XAttribute("type", "and"),
+
+                    new XElement("condition",
+                        new XAttribute("attribute", linkEntitySelfReferentialCondition.Attribute("attribute").Value),
+                        new XAttribute("operator", "ne"),
+                        new XAttribute("value", parentRecordId)
+                    ),
+
+                    new XElement("condition",
+                        new XAttribute("attribute", "new_tablefetchbcategory"),
+                        new XAttribute("operator", "eq"),
+                        new XAttribute("value", categoryOption.Value.ToString())
+                    )
+                );
+
+                #endregion
+
+                linkEntityElement.ReplaceWith(customFilter);
+
+                fetchExpression.Query = fetchXmlDocument.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                string errorMsg = ex.InnerException != null ? $"{ex.Message} Inner Exception: {ex.InnerException.Message}" : ex.Message;
+                throw new Exception(errorMsg, ex);
+            }
+        }
+    }
+}
